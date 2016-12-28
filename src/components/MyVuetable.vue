def8ed8146f785b3937e73b5f573a023f93772c9
Rati Wannapanop <rati.wannapanop@gmail.com>
2016-12-28 13:23:01 +0700

    lesson-3

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -1,7 +1,7 @@
 <template>
   <vuetable ref="vuetable"
     api-url="http://vuetable.ratiw.net/api/users"
-    :fields="['name', 'email', 'birthdate', 'address.line1', 'address.line2', 'address.zipcode']"
+    :fields="fields"
   ></vuetable>
 </template>
 
@@ -11,6 +11,14 @@ import Vuetable from 'vuetable-2/src/components/Vuetable'
 export default {
   components: {
     Vuetable
+  },
+  data () {
+    return {
+      fields: [
+        'name', 'email', 'birthdate',
+        'address.line1', 'address.line2', 'address.zipcode'
+      ]
+    }
   }
 }
 </script>
