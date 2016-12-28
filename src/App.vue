16d1d0d5f89d724178f7e6c1c31de30d727f7e5f
Rati Wannapanop <rati.wannapanop@gmail.com>
2016-12-28 12:59:04 +0700

    lesson-1

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div class="ui container">
+      <my-vuetable></my-vuetable>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyVuetable from './components/MyVuetable'
+
+export default {
+  name: 'app',
+  components: {
+    MyVuetable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyVuetable.vue
@@ -0,0 +1,16 @@
+<template>
+  <vuetable ref="vuetable"
+    api-url="http://vuetable.ratiw.net/api/users"
+    :fields="['name', 'email', 'birthdate']"
+  ></vuetable>
+</template>
+
+<script>
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+
+export default {
+  components: {
+    Vuetable
+  }
+}
+</script>
