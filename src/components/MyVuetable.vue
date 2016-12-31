b4b4feb450257a3416f18ec9a8ec996a6b3b2443
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-01-01 01:45:40 +0700

    add sortField to all fields

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -40,25 +40,36 @@ export default {
   data () {
     return {
       fields: [
-        'name', 'email',
+        {
+          name: 'name',
+          sortField: 'name'
+        },
+        {
+          name: 'email',
+          sortField: 'email'
+        },
         {
           name: 'birthdate',
+          sortField: 'birthdate',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
           callback: 'formatDate|DD-MM-YYYY'
         },
         {
           name: 'nickname',
+          sortField: 'nickname',
           callback: 'allcap'
         },
         {
           name: 'gender',
+          sortField: 'gender',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
           callback: 'genderLabel'
         },
         {
           name: 'salary',
+          sortField: 'salary',
           titleClass: 'center aligned',
           dataClass: 'right aligned',
           callback: 'formatNumber'
