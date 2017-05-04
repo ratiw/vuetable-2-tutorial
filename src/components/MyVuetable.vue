9075f6a97f46e1ee06c6db8a2ed4857a075665a4
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-05-04 17:11:48 +0700

    lesson-10

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -13,6 +13,8 @@
       :fields="fields"
       pagination-path=""
       :per-page="20"
+      :multi-sort="true"
+      :sort-order="sortOrder"
       @vuetable:pagination-data="onPaginationData"
     ></vuetable>
     <div class="vuetable-pagination ui basic segment grid">
@@ -41,28 +43,46 @@ export default {
   data () {
     return {
       fields: [
-        'name', 'email',
+        {
+          name: 'name',
+          sortField: 'name',
+        }, 
+        {
+          name: 'email',
+          sortField: 'email'
+        },
         {
           name: 'birthdate',
+          sortField: 'birthdate',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
           callback: 'formatDate|DD-MM-YYYY'
         },
         {
           name: 'nickname',
+          sortField: 'nickname',
           callback: 'allcap'
         },
         {
           name: 'gender',
+          sortField: 'gender',
           titleClass: 'center aligned',
           dataClass: 'center aligned',
           callback: 'genderLabel'
         },
         {
           name: 'salary',
+          sortField: 'salary',
           titleClass: 'center aligned',
           dataClass: 'right aligned',
           callback: 'formatNumber'
+        }
+      ],
+      sortOrder: [
+        {
+          field: 'email',
+          sortField: 'email',
+          direction: 'asc'
         }
       ]
     }
