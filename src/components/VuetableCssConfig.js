a33b2a57530e6b5b38c5abff8e6663081dc3726d
Rati Wannapanop <rati.wannapanop@gmail.com>
2017-06-05 15:47:15 +0700

    add renderIcon function

--- src/components/VuetableCssConfig.js
+++ src/components/VuetableCssConfig.js
@@ -1,10 +1,12 @@
 export default {
   table: {
     tableClass: "table table-striped table-bordered",
-    loadingClass: "loading",
     ascendingIcon: "glyphicon glyphicon-chevron-up",
     descendingIcon: "glyphicon glyphicon-chevron-down",
     handleIcon: "glyphicon glyphicon-menu-hamburger",
+    renderIcon: function(classes, options) {
+      return `<span class="${classes.join(' ')}"></span>`
+    }
   },
   paginationInfo: {
     infoClass: 'pull-left'
